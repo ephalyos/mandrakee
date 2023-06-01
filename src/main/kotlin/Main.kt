f2cf3acbd7d940ed41dd014f3abe45439699c8d7
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-01 11:58:38 -0600

    Add file handling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,26 @@
 
+import java.io.File
+
 fun main (args: Array<String>) {
-
-    println("args size: ${args.size}")
-    println(args.joinToString(" "))
-
-    val lines = listOf(
-        "var( value( = 0 // line-comment, should not see any of this\n",
-        "this val /* should not see any of this */ val new += 1\n",
-        "13 13e14 13e+14 13e-14 13.14 13.14e15 13.14e+15 13.14e-15\n"
-    )
 
     val scanner = Scanner()
 
-    val tokens = scanner.scan(lines)
-
-    for (token in tokens) {
-        println(token.toString())
+    if (args.isEmpty()) {
+        while (true) {
+            print(">> ")
+            val line = readln()
+            if (line == "exit()") break
+            val tokens = scanner.scan(listOf(line))
+            for (token in tokens) {
+                println(token.toString())
+            }
+        }
+    } else {
+        val input: List<String> = File(args[0]).readLines()
+        val tokens = scanner.scan(input)
+        for (token in tokens) {
+            println(token.toString())
+        }
     }
 
 }
