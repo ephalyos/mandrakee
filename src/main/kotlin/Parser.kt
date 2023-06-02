563df559357d1e9c14777f4279ea5499f2df2fc7
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-02 03:20:35 -0600

    Finish adding non-terminals methods

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -3,7 +3,7 @@ class Parser (
     private val tokens: List<Token>
 ) {
 
-    private val eolT = Token(lexeme = "", type = TokenType.EOF)
+    private val eofT = Token(lexeme = "", type = TokenType.EOF)
     private val classT = Token(lexeme = "class", type = TokenType.CLASS)
     private val identifierT = Token(lexeme = "", type = TokenType.IDENTIFIER)
     private val funT = Token(lexeme = "fun", type = TokenType.FUN)
@@ -24,8 +24,8 @@ class Parser (
     private val andT = Token(lexeme = "and", type = TokenType.AND)
     private val notEqualsT = Token(lexeme = "!=", type = TokenType.NOT_EQUALS)
     private val equalsT = Token(lexeme = "==", type = TokenType.EQUALS)
-    private val moreThanT = Token(lexeme = ">", type = TokenType.GREATER_THAN)
-    private val moreEqualThanT = Token(lexeme = ">=", type = TokenType.GREATER_EQUAL_THAN)
+    private val greaterThanT = Token(lexeme = ">", type = TokenType.GREATER_THAN)
+    private val greaterEqualThanT = Token(lexeme = ">=", type = TokenType.GREATER_EQUAL_THAN)
     private val lessThanT = Token(lexeme = "<", type = TokenType.LESSER_THAN)
     private val lessEqualThanT = Token(lexeme = "<=", type = TokenType.LESSER_EQUAL_THAN)
     private val minusT = Token(lexeme = "-", type = TokenType.SUBSTRACT)
@@ -45,21 +45,21 @@ class Parser (
     private val commaT = Token(lexeme = ",", type = TokenType.COMMA)
 
     private var i = 0
-    private var errorFound = false
     private var current = Token(null, 0, "", TokenType.EOF)
 
     fun parse () {
         current = tokens[i]
         program()
+        if (current == eofT) {
+            println("Valid Program")
+        }
     }
 
     private fun match (t: Token) {
-        if (errorFound) return
         if (current.type == t.type){
             i++
             current = tokens[i]
         } else {
-            errorFound = true
             throw Error("Error at ${current.line} expected a ${t.type.name}")
         }
     }
@@ -97,9 +97,9 @@ class Parser (
             match(classT)
             match(identifierT)
             classInher()
-            match(leftParenthesesT)
+            match(leftBraceT)
             functions()
-            match(rightParenthesesT)
+            match(rightBraceT)
         } else {
             throw Error("Error at ${current.line} expected CLASS")
         }
@@ -109,8 +109,6 @@ class Parser (
         if (current == lessThanT){
             match(lessThanT)
             match(identifierT)
-        } else {
-            throw Error("Error at ${current.line} expected IDENTIFIER")
         }
     }
 
@@ -118,6 +116,8 @@ class Parser (
         if (current == funT) {
             match(funT)
             function()
+        } else {
+            throw Error("Error at ${current.line} expected FUNCTION")
         }
     }
 
@@ -166,7 +166,7 @@ class Parser (
                 block()
             }
             else -> {
-                throw Error("Error at ${current.line}")
+                throw Error("Error at ${current.line} expected STATEMENT")
             }
         }
     }
@@ -179,7 +179,7 @@ class Parser (
                 match(semicolonT)
             }
             else -> {
-                throw Error("Error at ${current.line}")
+                throw Error("Error at ${current.line} expected EXPRESSION")
             }
         }
     }
@@ -211,7 +211,7 @@ class Parser (
                 match(semicolonT)
             }
             else -> {
-                throw Error("Error at ${current.line} expected VARIABLE | EXPRESSION")
+                throw Error("Error at ${current.line} expected FOR STATEMENT 1")
             }
         }
     }
@@ -227,7 +227,7 @@ class Parser (
                 match(semicolonT)
             }
             else -> {
-                throw Error("Error at ${current.line} expected ;")
+                throw Error("Error at ${current.line} expected FOR STATEMENT 2")
             }
         }
     }
@@ -250,7 +250,7 @@ class Parser (
             statement()
             elseStatement()
         } else {
-            throw Error("Error at ${current.line} expected IF EXPRESSION")
+            throw Error("Error at ${current.line} expected IF")
         }
     }
 
@@ -267,7 +267,7 @@ class Parser (
             expression()
             match(semicolonT)
         } else {
-            throw Error("Error at ${current.line} expected PRINT EXPRESSION")
+            throw Error("Error at ${current.line} expected PRINT")
         }
     }
 
@@ -277,7 +277,7 @@ class Parser (
             returnExpressionOptional()
             match(semicolonT)
         } else {
-            throw Error("Error at ${current.line} expected RETURN EXPRESSION")
+            throw Error("Error at ${current.line} expected RETURN")
         }
     }
 
@@ -298,7 +298,7 @@ class Parser (
             match(rightParenthesesT)
             statement()
         } else {
-            throw Error("Error at ${current.line} expected WHILE EXPRESSION")
+            throw Error("Error at ${current.line} expected WHILE")
         }
     }
 
@@ -308,7 +308,7 @@ class Parser (
             blockDecl()
             match(rightBraceT)
         } else {
-            throw Error("Error at ${current.line} expected {")
+            throw Error("Error at ${current.line} expected BLOCK")
         }
     }
 
@@ -339,7 +339,7 @@ class Parser (
                 assignmentOptional()
             }
             else -> {
-                throw Error("Error at ${current.line} expected EXPRESSION")
+                throw Error("Error at ${current.line} expected ASSIGNMENT")
             }
         }
     }
@@ -352,7 +352,7 @@ class Parser (
                 logicOr2()
             }
             else -> {
-                throw Error("Error at ${current.line} expected EXPRESSION")
+                throw Error("Error at ${current.line} expected LOGIC_OR")
             }
         }
     }
@@ -380,7 +380,7 @@ class Parser (
                 logicAnd2()
             }
             else -> {
-                throw Error("Error at ${current.line} expected EXPRESSION")
+                throw Error("Error at ${current.line} expected LOGIC_AND")
             }
         }
     }
@@ -401,8 +401,276 @@ class Parser (
                 equality2()
             }
             else -> {
-                throw Error("Error at ${current.line} expected EXPRESSION")
+                throw Error("Error at ${current.line} expected EQUALITY")
             }
+        }
+    }
+
+    private fun equality2 () {
+        when (current) {
+            notEqualsT -> {
+                match(notEqualsT)
+                comparison()
+                equality2()
+            }
+            equalsT -> {
+                match(equalsT)
+                comparison()
+                equality2()
+            }
+        }
+    }
+
+    private fun comparison () {
+        when (current) {
+            notT, minusT, trueT, falseT, nullT, thisT, integerT, doubleT,
+            stringT, identifierT, leftParenthesesT, superT-> {
+                term()
+                comparison2()
+            }
+            else -> {
+                throw Error("Error at ${current.line} expected COMPARISON")
+            }
+        }
+    }
+
+    private fun comparison2 () {
+        when (current) {
+            greaterThanT -> {
+                match(greaterThanT)
+                term()
+                comparison2()
+            }
+            greaterEqualThanT -> {
+                match(greaterEqualThanT)
+                term()
+                comparison2()
+            }
+            lessThanT -> {
+                match(lessThanT)
+                term()
+                comparison2()
+            }
+            lessEqualThanT -> {
+                match(lessEqualThanT)
+                term()
+                comparison2()
+            }
+        }
+    }
+
+    private fun term () {
+        when (current) {
+            notT, minusT, trueT, falseT, nullT, thisT, integerT, doubleT,
+            stringT, identifierT, leftParenthesesT, superT-> {
+                factor()
+                term2()
+            }
+            else -> {
+                throw Error("Error at ${current.line} expected TERM")
+            }
+        }
+    }
+
+    private fun term2 () {
+        when (current) {
+            minusT -> {
+                match(minusT)
+                factor()
+                term2()
+            }
+            plusT -> {
+                match(plusT)
+                factor()
+                term2()
+            }
+        }
+    }
+
+    private fun factor () {
+        when (current) {
+            notT, minusT, trueT, falseT, nullT, thisT, integerT, doubleT,
+            stringT, identifierT, leftParenthesesT, superT-> {
+                unary()
+                factor2()
+            }
+            else -> {
+                throw Error("Error at ${current.line} expected FACTOR")
+            }
+        }
+    }
+
+    private fun factor2 () {
+        when (current) {
+            divisionT -> {
+                match(divisionT)
+                unary()
+                factor2()
+            }
+            multiplicationT -> {
+                match(multiplicationT)
+                unary()
+                factor2()
+            }
+        }
+    }
+
+    private fun unary () {
+        when (current) {
+            notT -> {
+                match(notT)
+                unary()
+            }
+            minusT -> {
+                match(minusT)
+                unary()
+            }
+            trueT, falseT, nullT, thisT, integerT, doubleT,
+            stringT, identifierT, leftParenthesesT, superT-> {
+                call()
+            }
+            else -> {
+                throw Error("Error at ${current.line} expected UNARY")
+            }
+        }
+    }
+
+    private fun call () {
+        when (current) {
+            trueT, falseT, nullT, thisT, integerT, doubleT,
+            stringT, identifierT, leftParenthesesT, superT-> {
+                primary()
+                call2()
+            }
+            else -> {
+                throw Error("Error at ${current.line} expected CALL")
+            }
+        }
+    }
+
+    private fun call2 () {
+        when (current) {
+            leftParenthesesT -> {
+                match(leftParenthesesT)
+                argumentsOptional()
+                match(rightParenthesesT)
+                call2()
+            }
+            dotT -> {
+                match(dotT)
+                match(identifierT)
+                call2()
+            }
+        }
+    }
+
+    private fun callOptional () {
+        when (current) {
+            trueT, falseT, nullT, thisT, integerT, doubleT,
+            stringT, identifierT, leftParenthesesT, superT-> {
+                call()
+                match(dotT)
+            }
+        }
+    }
+
+    private fun primary () {
+        when (current) {
+            trueT -> match(trueT)
+            falseT -> match(falseT)
+            nullT -> match(nullT)
+            thisT -> match(thisT)
+            integerT -> match(integerT)
+            doubleT -> match(doubleT)
+            stringT -> match(stringT)
+            identifierT -> match(identifierT)
+            leftParenthesesT -> {
+                match(leftParenthesesT)
+                expression()
+                match(rightParenthesesT)
+            }
+            superT -> {
+                match(superT)
+                match(dotT)
+                match(identifierT)
+            }
+            else -> {
+                throw Error("Error at ${current.line} expected PRIMARY")
+            }
+        }
+    }
+
+    // OTHERS  - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    private fun function () {
+        if (current == identifierT) {
+            match(identifierT)
+            match(leftParenthesesT)
+            parametersOptional()
+            match(rightParenthesesT)
+            block()
+        } else {
+            throw Error("Error at ${current.line} expected FUNCTION")
+        }
+    }
+
+    private fun functions () {
+        if (current == identifierT) {
+            function()
+            functions()
+        }
+    }
+
+    private fun parametersOptional () {
+        if (current == identifierT) {
+            parameters()
+        }
+    }
+
+    private fun parameters () {
+        if (current == identifierT) {
+            match(identifierT)
+            parameters2()
+        } else {
+            throw Error("Error at ${current.line} expected PARAMETERS")
+        }
+    }
+
+    private fun parameters2 () {
+        if (current == commaT) {
+            match(commaT)
+            match(identifierT)
+            parameters2()
+        }
+    }
+
+    private fun argumentsOptional () {
+        when (current) {
+            notT, minusT, trueT, falseT, nullT, thisT, integerT, doubleT,
+            stringT, identifierT, leftParenthesesT, superT-> {
+                arguments()
+            }
+        }
+    }
+
+    private fun arguments () {
+        when (current) {
+            notT, minusT, trueT, falseT, nullT, thisT, integerT, doubleT,
+            stringT, identifierT, leftParenthesesT, superT-> {
+                expression()
+                arguments2()
+            }
+            else -> {
+                throw Error("Error at ${current.line} expected ARGUMENTS")
+            }
+        }
+    }
+
+    private fun arguments2 () {
+        if (current == commaT) {
+            match(commaT)
+            expression()
+            arguments2()
         }
     }
 
