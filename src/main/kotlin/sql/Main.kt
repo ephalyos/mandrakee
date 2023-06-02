a99f0da6ef0c76664dba8ca6f51b3295edf6b528
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-02 16:12:53 -0600

    Add Scanner instance

--- src/main/kotlin/sql/Main.kt
+++ src/main/kotlin/sql/Main.kt
@@ -1,12 +1,18 @@
 
-
+package sql
 
 fun main () {
+
+    val scanner = Scanner()
 
     while (true) {
         print(">> ")
         val line = readln()
         if (line == "exit()") break
+        val tokens = scanner.scan(line)
+        for (token in tokens) {
+            println(token.toString())
+        }
     }
 
 }
