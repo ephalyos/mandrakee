7a62ba93b34448b45a9de0f93a6fbe475c8fb5dc
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-02 15:13:17 -0600

    Add Token class to sql package

---
+++ src/main/kotlin/sql/Token.kt
@@ -0,0 +1,39 @@
+
+class Token (
+    val type: TokenType,
+    val lexeme: String,
+    val line: Int = 0
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Token){
+            return false
+        }
+        return this.type == other.type
+    }
+
+    override fun hashCode(): Int {
+        var result = type.hashCode()
+        result = 31 * result + lexeme.hashCode()
+        result = 31 * result + line
+        return result
+    }
+
+    override fun toString(): String = "Token: ${type.name} with lexeme: $lexeme found at: $line"
+
+}
+
+enum class TokenType {
+
+    IDENTIFIER,
+
+    SELECT,
+    FROM,
+    DISTINCT,
+    COMMA,
+    DOT,
+    ASTERISK,
+
+    EOF
+
+}
