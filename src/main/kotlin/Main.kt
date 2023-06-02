41a696ae92ca3d3044108a590ba2b30d85b5830b
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-02 03:20:10 -0600

    Create Parser object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,16 +11,14 @@ fun main (args: Array<String>) {
             val line = readln()
             if (line == "exit()") break
             val tokens = scanner.scan(listOf(line))
-            for (token in tokens) {
-                println(token.toString())
-            }
+            val parser = Parser(tokens)
+            parser.parse()
         }
     } else {
         val input: List<String> = File(args[0]).readLines()
         val tokens = scanner.scan(input)
-        for (token in tokens) {
-            println(token.toString())
-        }
+        val parser = Parser(tokens)
+        parser.parse()
     }
 
 }
