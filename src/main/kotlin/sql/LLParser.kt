b37f1e04bd37b0094765ad6ba6aab9885c5985a4
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-05 04:52:28 -0600

    Fix out of bounds error

--- src/main/kotlin/sql/LLParser.kt
+++ src/main/kotlin/sql/LLParser.kt
@@ -5,17 +5,6 @@ class LLParser (
     private val tokens: List<Token>
 ) {
 
-    private val terminals = hashMapOf(
-        "id" to TokenType.IDENTIFIER,
-        "select" to TokenType.SELECT,
-        "from" to TokenType.FROM,
-        "distinct" to TokenType.DISTINCT,
-        "," to TokenType.COMMA,
-        "." to TokenType.DOT,
-        "*" to TokenType.ASTERISK,
-        "$" to TokenType.EOF
-    )
-
     private val table = hashMapOf(
         "Q" to hashMapOf(
             "select" to listOf("select", "D", "from", "T")
@@ -61,26 +50,41 @@ class LLParser (
         )
     )
 
-    fun lookup (nonTerminal: String, terminal: String): List<String> {
-        return table[nonTerminal]?.get(terminal)?.reversed()!!
+    private fun lookup (nonTerminal: String, terminal: String): List<String> {
+        return table[nonTerminal]?.get(terminal)?.reversed() ?: throw Error("Error at symbol: $terminal")
     }
 
     private var stack = mutableListOf("$", "Q")
 
     private var i = 0
 
-    private fun getNextToken (): Token {
-        return tokens[++i]
+    private fun getNextSymbol(): String {
+        val nextToken = tokens[i++]
+        return if (nextToken.type == TokenType.IDENTIFIER) "id" else nextToken.lexeme
     }
 
     fun parse () {
-        while (true) {
-            val stackTop = stack.removeAt(stack.size - 1)
-            val inputToken = getNextToken()
-            val terminal = if (inputToken.type == TokenType.IDENTIFIER) "id" else inputToken.lexeme
-            val derivation = lookup(stackTop, terminal)
-            break
+
+        var currentSymbol = getNextSymbol()
+
+        while (stack.isNotEmpty()) {
+
+            println("current stack: $stack current symbol: $currentSymbol")
+
+            val top = stack.removeAt(stack.size - 1)
+
+            if (top == currentSymbol) {
+                if (top == "$") break
+                currentSymbol = getNextSymbol()
+            } else {
+                val derivation = lookup(top, currentSymbol)
+                stack.addAll(derivation)
+            }
+
         }
+
+        println("Valid query")
+
     }
 
 }
