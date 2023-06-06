a8c346c5b0909bf5325f3daa8acb24df42145bd9
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-05 19:33:39 -0600

    No token print

--- src/main/kotlin/sql/Main.kt
+++ src/main/kotlin/sql/Main.kt
@@ -22,9 +22,6 @@ fun main () {
         val line = readln()
         if (line == "exit()") break
         val tokens = scanner.scan(line)
-        for (token in tokens) {
-            println(token.toString())
-        }
         val parser = LRParser(tokens)
         parser.parse()
     }
