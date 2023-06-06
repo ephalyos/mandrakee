6e945fa4d7166e2c8ad8dfe5c46fc7811d840bd0
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-05 18:51:44 -0600

    Add action and goto tables

--- src/main/kotlin/sql/LRParser.kt
+++ src/main/kotlin/sql/LRParser.kt
@@ -1,8 +1,189 @@
 
 package sql
 
+import com.sun.tools.example.debug.expr.ExpressionParserConstants.DO
+
 class LRParser (
     private val tokens: List<Token>
 ) {
+
+    private val productions = hashMapOf(
+        1 to Pair("Q", listOf("select", "D", "from", "T")),
+        2 to Pair("D", listOf("distinct", "P")),
+        3 to Pair("D", listOf("P")),
+        4 to Pair("P", listOf("*")),
+        5 to Pair("P", listOf("A")),
+        6 to Pair("A", listOf("A2", "A1")),
+        7 to Pair("A1", listOf(",", "A")),
+        8 to Pair("A1", listOf()),
+        9 to Pair("A2", listOf("id", "A3")),
+        10 to Pair("A3", listOf(".", "id")),
+        11 to Pair("A3", listOf()),
+        12 to Pair("T", listOf("T2", "T1")),
+        13 to Pair("T1", listOf(",", "T")),
+        14 to Pair("T1", listOf()),
+        15 to Pair("T2", listOf("id", "T3")),
+        16 to Pair("T3", listOf("id")),
+        17 to Pair("T3", listOf())
+    )
+
+    enum class ACTION {
+        SHIFT,
+        REDUCE,
+        ACCEPT
+    }
+    private val s = ACTION.SHIFT
+    private val r = ACTION.REDUCE
+    private val acc = ACTION.ACCEPT
+
+    private val actionTable = hashMapOf(
+        0 to hashMapOf(
+            "select" to Pair(s, 2),
+        ),
+        1 to hashMapOf(
+            "$" to Pair(acc, 0),
+        ),
+        2 to hashMapOf(
+            "distinct" to Pair(s, 4),
+            "*" to Pair(s, 6),
+            "id" to Pair(s, 9),
+        ),
+        3 to hashMapOf(
+            "from" to Pair(s, 10),
+        ),
+        4 to hashMapOf(
+            "*" to Pair(s, 6),
+            "id" to Pair(s, 9),
+        ),
+        5 to hashMapOf(
+            "from" to Pair(r, 3),
+        ),
+        6 to hashMapOf(
+            "from" to Pair(r, 4),
+        ),
+        7 to hashMapOf(
+            "from" to Pair(r, 5),
+        ),
+        8 to hashMapOf(
+            "from" to Pair(r, 8),
+            "," to Pair(s, 13),
+        ),
+        9 to hashMapOf(
+            "from" to Pair(r, 11),
+            "," to Pair(r, 11),
+            "." to Pair(s, 15),
+        ),
+        10 to hashMapOf(
+            "id" to Pair(s, 18),
+        ),
+        11 to hashMapOf(
+            "from" to Pair(r, 2),
+        ),
+        12 to hashMapOf(
+            "from" to Pair(r, 6),
+        ),
+        13 to hashMapOf(
+            "id" to Pair(s, 9),
+        ),
+        14 to hashMapOf(
+            "from" to Pair(r, 9),
+            "," to Pair(r, 9),
+        ),
+        15 to hashMapOf(
+            "id" to Pair(s, 20),
+        ),
+        16 to hashMapOf(
+            "$" to Pair(r, 1),
+        ),
+        17 to hashMapOf(
+            "," to Pair(s, 22),
+            "$" to Pair(r, 14),
+        ),
+        18 to hashMapOf(
+            "," to Pair(r, 17),
+            "id" to Pair(s, 24),
+            "$" to Pair(r, 17),
+        ),
+        19 to hashMapOf(
+            "from" to Pair(r, 7),
+        ),
+        20 to hashMapOf(
+            "from" to Pair(r, 10),
+            "," to Pair(r, 10),
+        ),
+        21 to hashMapOf(
+            "$" to Pair(r, 12),
+        ),
+        22 to hashMapOf(
+            "id" to Pair(s, 18),
+        ),
+        23 to hashMapOf(
+            "," to Pair(r, 15),
+            "$" to Pair(r, 15),
+        ),
+        24 to hashMapOf(
+            "," to Pair(r, 16),
+            "$" to Pair(r, 16),
+        ),
+        25 to hashMapOf(
+            "$" to Pair(r, 13),
+        ),
+    )
+
+    private val gotoTable = hashMapOf(
+        0 to hashMapOf(
+            "Q" to 1
+        ),
+        2 to hashMapOf(
+            "D" to 3,
+            "P" to 5,
+            "A" to 7,
+            "A2" to 8
+        ),
+        4 to hashMapOf(
+            "P" to 11,
+            "A" to 7,
+            "A2" to 8
+        ),
+        8 to hashMapOf(
+            "A1" to 12
+        ),
+        9 to hashMapOf(
+            "A3" to 14
+        ),
+        10 to hashMapOf(
+            "T" to 16,
+            "T2" to 17
+        ),
+        13 to hashMapOf(
+            "A" to 19,
+            "A2" to 8
+        ),
+        17 to hashMapOf(
+            "T1" to 21
+        ),
+        18 to hashMapOf(
+            "T3" to 23
+        ),
+        22 to hashMapOf(
+            "T" to 25,
+            "T2" to 17
+        ),
+    )
+
+    private fun actionLookup (state: Int, terminal: String): Pair<ACTION, Int> {
+        return actionTable[state]?.get(terminal)!!
+    }
+
+    private fun gotoLookup (state: Int, nonTerminal: String): Int {
+        return 0
+    }
+
+    private var stateStack = mutableListOf(0)
+    private var symbolStack = mutableListOf<String>()
+
+    private fun parse () {
+
+    }
 
 }
