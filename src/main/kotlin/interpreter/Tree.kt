701ea8c6526726f72ddc6cea8a060d1e0f010ec3
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-25 20:01:58 -0600

    Add getChild() method

--- src/main/kotlin/interpreter/Tree.kt
+++ src/main/kotlin/interpreter/Tree.kt
@@ -1,17 +1,24 @@
 
 package interpreter
 
-class Node (
-    val token: Token
+class Node<T> (
+    val value: T,
+    var depth: Int = 0,
 ) {
-    val children = mutableListOf<Node>()
-    fun addChild ( child: Node ) {
-        children.add(child)
-    }
-}
 
-class Tree (
-    val node: Node
-) {
+    val children = mutableListOf<Node<T>>()
+
+    fun addChild ( node: Node<T> ) {
+        node.depth = depth + 1
+        children.add(node)
+    }
+
+    fun getChild () {
+        for (child in children) {
+            val spaces = "--".repeat(child.depth)
+            println("$spaces${child.value}")
+            child.getChild()
+        }
+    }
 
 }
