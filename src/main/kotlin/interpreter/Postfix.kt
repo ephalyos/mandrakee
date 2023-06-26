daba3875ad70be51c60be549bba07365cc14b370
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-25 23:31:48 -0600

    Finish toPostfix() method

--- src/main/kotlin/interpreter/Postfix.kt
+++ src/main/kotlin/interpreter/Postfix.kt
@@ -2,19 +2,22 @@
 package interpreter
 
 class Postfix (
-    val infix: List<Token>
+    private val infix: List<Token>
 ) {
-    val postfix = mutableListOf<Token>()
-    val stack = mutableListOf<Token>()
-    val controlStack = mutableListOf<Token>()
+    private val postfix = mutableListOf<Token>()
+    private val stack = mutableListOf<Token>()
+    private val controlStack = mutableListOf<Token>()
 
-    fun toPostfix () {
+    fun toPostfix (): List<Token> {
+
         var inControl = false
+        var index = 0
+
         for (token in infix) {
+            index++
             if (token.type == TokenType.EOF) {
                break
             }
-
             else if (token.isReserved()) {
                 postfix.add(token)
                 if (token.isControl()) {
@@ -22,23 +25,61 @@ class Postfix (
                     controlStack.add(token)
                 }
             }
-
             else if (token.isOperand()) {
                postfix.add(token)
             }
-
-            else if (token.type == TokenType.LEFT_PARENTHESES){
-               stack.add(token)
-            }
-
-            else if (token.isOperator()){
-               while (stack.size > 0 && stack[stack.size-1].precedence() >= token.precedence()){
-                   postfix.add(stack.removeAt(stack.size-1))
+            else if (token.isOperator()) {
+               while (stack.isNotEmpty() && stack.last().precedence() >= token.precedence()) {
+                   postfix.add(stack.removeLast())
                }
                stack.add(token)
             }
-
+            else if (token.type == TokenType.LEFT_PARENTHESES) {
+                stack.add(token)
+            }
+            else if (token.type == TokenType.RIGHT_PARENTHESES) {
+                while (stack.isNotEmpty() && stack.last().type != TokenType.LEFT_PARENTHESES) {
+                    postfix.add(stack.removeLast())
+                }
+                if (stack.last().type == TokenType.LEFT_PARENTHESES)
+                    stack.removeLast()
+                if (inControl) {
+                    postfix.add(Token(type = TokenType.SEMI_COLON, lexeme = ";"))
+                }
+            }
+            else if (token.type == TokenType.LEFT_BRACE) {
+                stack.add(token)
+            }
+            else if (token.type == TokenType.RIGHT_BRACE && inControl) {
+                if (infix[index].type == TokenType.ELSE) {
+                    stack.removeLast()
+                }
+                else {
+                    stack.removeLast()
+                    postfix.add(Token(type = TokenType.SEMI_COLON, lexeme = ";"))
+                    controlStack.removeLast()
+                    if (controlStack.isEmpty())
+                        inControl = false
+                }
+            }
+            else if (token.type == TokenType.SEMI_COLON) {
+                while (stack.isNotEmpty() && stack.last().type != TokenType.LEFT_BRACE) {
+                    postfix.add(stack.removeLast())
+                }
+                postfix.add(token)
+            }
         }
+
+        while (stack.isNotEmpty())
+            postfix.add(stack.removeLast())
+
+        while (controlStack.isNotEmpty()) {
+            controlStack.removeLast()
+            postfix.add(Token(type = TokenType.SEMI_COLON, lexeme = ";"))
+        }
+
+        return postfix
+
     }
 
 }
