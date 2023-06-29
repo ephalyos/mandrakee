8ea5e919d9e9ba55cdd238adb374ce95bab83582
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-29 13:00:51 -0600

    Add Parser check

--- src/main/kotlin/interpreter/Main.kt
+++ src/main/kotlin/interpreter/Main.kt
@@ -6,36 +6,51 @@ fun main (args: Array<String>) {
 
     if (args.isEmpty()) {
 
-        // " if (a == 5) { b = 6; } else { c = 8; } var b = 7; var c; "
-        // " if (true) { a = 5; } "
-        // " var nombre = \"Carlos\"; var apellido = \"Herrera\"; print nombre + \" \" + apellido; "
-        // " print \"el área es: \"; \n print area; "
-        // " if (area > 100 and radius < 50) { print \"valid\"; } else { print \"invalid\"; } "
-        // " while ( init <= lim ) { print fib; aux = aux + fib; } print \"finished\"; "
-        // " for ( init = 1 ; init <= lim ; init = init + 1 ) { print fib; aux = aux + fib; } print \"finished\"; "
+        while ( true ) {
 
-        val input = " print 5 + 4; print \"hello\"; "
+            // Line Reading
+            print(">> ")
+            val line = readln()
+            if ( line == "exit()") break
 
-        val tokens = Scanner(listOf(input)).scan()
-        val postfix = Postfix(tokens).toPostfix()
+            // Infix Tokens
+            val tokens = Scanner(listOf(line)).scan()
 
-        val root = AST(postfix).toAST()
-        root.printChildren()
-        root.run()
+            // Parser Check
+            Parser(tokens).parse()
+
+            // Postfix Tokens
+            val postfix = Postfix(tokens).toPostfix()
+
+            // AST Creation
+            val root = AST(postfix).toAST()
+
+            // Execution
+//             root.printChildren()
+            root.run()
+
+        }
 
     } else {
 
         // File Reading
         val input: List<String> = File(args[0]).readLines()
 
-        // Scanner creation
-        val scanner = Scanner(input)
-        val tokens = scanner.scan()
-        for (token in tokens) println(token.toString())
+        // Infix Tokens
+        val tokens = Scanner(input).scan()
 
-        // Parser creation
-        val parser = Parser(tokens)
-        parser.parse()
+        // Parser Check
+        Parser(tokens).parse()
+
+        // Postfix Tokens
+        val postfix = Postfix(tokens).toPostfix()
+
+        // AST Creation
+        val root = AST(postfix).toAST()
+
+        // Execution
+//        root.printChildren()
+        root.run()
 
     }
 
