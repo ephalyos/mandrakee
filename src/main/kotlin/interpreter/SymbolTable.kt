1865c3acafb9aee2a2d007391511c29aa88d9722
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-28 20:40:14 -0600

    Add management operations

--- src/main/kotlin/interpreter/SymbolTable.kt
+++ src/main/kotlin/interpreter/SymbolTable.kt
@@ -1,6 +1,28 @@
 package interpreter
 
 class SymbolTable {
+    companion object {
 
-    val table = mutableMapOf<String, Any>()
+        private val table = mutableMapOf<String, Any>()
+        fun retrieve ( key: String ): Any? {
+            if (table.contains(key))
+                return table[key]
+            return null
+        }
+
+        fun update ( key: String, value: Any ) {
+            if (table.contains(key))
+                table[key] = value
+            else
+                throw Error("Identifier $key is undefined")
+        }
+
+        fun add ( key: String, value: Any ) {
+            if (table.contains(key))
+                throw Error("Identifier $key is already defined")
+            else
+                table[key] = value
+        }
+
+    }
 }
